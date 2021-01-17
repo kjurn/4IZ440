240a070c80afa48afbc6d7c15d848ba3a92475b7
Jurajko <juroszucs@gmail.com>
2021-01-17 12:21:36 +0100

    deleted wrong project

--- untitled/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String [] args) {
-        NumberFunctions<Integer> iOb = new NumberFunctions<>(6);
-        NumberFunctions<Double> dOb = new NumberFunctions<>(-6.0);
-
-        iOb.absEqual(dOb);
-    }
-}
